916e8455c94a99480b4e3c819e78561755bd164c
nguyetnong <nguyetnong71099@gmail.com>
2022-11-06 13:37:30 +0700

    update file main

--- Main_tinhchuvi.py
+++ Main_tinhchuvi.py
@@ -1,15 +1,20 @@
 import tinhchuvi
 
-giatri = tinhchuvi.Tinh_Chu_Vi(chieudai = 12,chieurong = 23)
-giatri2 = tinhchuvi.Tinh_Dien_tich(chieudai = 12,chieurong = 23)
+dai=float(input("nhap chieu đai can tinh:"))
+rong=float(input("nhap chieu rong can tinh:"))
+bankinh=float(input("nhap ban kinh:"))
+toado=float(input("nhap goc can tinh:"))
+               
+giatri = tinhchuvi.Tinh_Chu_Vi(chieudai = dai,chieurong = rong)
+giatri2 = tinhchuvi.Tinh_Dien_tich(chieudai = dai,chieurong = rong)
 print (giatri)
 print (giatri2)
 
-vetron=tinhchuvi.hinhtron(r=100)
-print(vetron)
+tinhchuvi.hinhtron(r=bankinh)
 
-tron=tinhchuvi.chuvi_hinhtron(r=100,goc=15)
-tron2=tinhchuvi.tinhdientich(r=100,goc=15)
+
+tron=tinhchuvi.chuvi_hinhtron(r=bankinh,goc=toado)
+tron2=tinhchuvi.tinhdientich(r=bankinh,goc=toado)
 
 print(tron)
 print(tron2)
